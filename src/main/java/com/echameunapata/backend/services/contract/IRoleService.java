7add77d9f1169ceeb9d53a394bba31c20fcf164d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-10-31 10:16:20 -0600

    :sparkles: feat: add new method for role services

--- src/main/java/com/echameunapata/backend/services/contract/IRoleService.java
+++ src/main/java/com/echameunapata/backend/services/contract/IRoleService.java
@@ -2,6 +2,11 @@ package com.echameunapata.backend.services.contract;
 
 import com.echameunapata.backend.domain.models.Role;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface IRoleService {
     Role findById(String id);
+    List<Role> findAllRoles();
+    List<Role> findAllByIds(List<String>ids);
 }
--- src/main/java/com/echameunapata/backend/services/impl/RoleServiceImpl.java
+++ src/main/java/com/echameunapata/backend/services/impl/RoleServiceImpl.java
@@ -7,6 +7,9 @@ import com.echameunapata.backend.services.contract.IRoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class RoleServiceImpl implements IRoleService {
 
@@ -26,7 +29,6 @@ public class RoleServiceImpl implements IRoleService {
     @Override
     public Role findById(String id) {
         try{
-            System.out.println("Role found: " + id);
             var role = roleRepository.findById(id).orElse(null);
             if (role == null)
             {
@@ -35,6 +37,45 @@ public class RoleServiceImpl implements IRoleService {
             return role;
         }catch (Exception e){
             throw  e;
+        }
+    }
+
+    /**
+     * Este método permite trear todos los roles almacenados.
+     *
+     * @return La lista de roles almacenada.
+     * @throws HttpError Si la lista esta vacia.
+     */
+    @Override
+    public List<Role> findAllRoles() {
+        try{
+            var roles = roleRepository.findAll();
+            if (roles.isEmpty()){
+                throw new HttpError(HttpStatus.FOUND, "Role list is empty");
+            }
+            return roles;
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    /**
+     * Este método busca una lista de roles por sus IDs.
+     *
+     * @param ids La lista de IDs de los roles a buscar.
+     * @return La lista de roles encontrados.
+     * @throws HttpError En excepciones internas del ide.
+     */
+    @Override
+    public List<Role> findAllByIds(List<String> ids) {
+        try{
+            Iterable<Role>lisRoles = roleRepository.findAllById(ids);
+            List<Role>roles = new ArrayList<>();
+            lisRoles.forEach(roles::add);
+
+            return roles;
+        }catch (Exception e){
+            throw e;
         }
     }
 }
