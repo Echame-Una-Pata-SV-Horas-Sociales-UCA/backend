5ad9d80d17dd46b4070324401ad2c4700467694a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-12-03 21:06:47 -0600

    public route

--- src/main/java/com/echameunapata/backend/configurations/auth/WebSecurityConfiguration.java
+++ src/main/java/com/echameunapata/backend/configurations/auth/WebSecurityConfiguration.java
@@ -71,7 +71,8 @@ public class WebSecurityConfiguration {
             auth.requestMatchers(
                     apiBasePath + "/auth/**",
                     apiBasePath +"/reports/create",
-                    apiBasePath + "/evidence/create/**"
+                    apiBasePath + "/evidence/create/**",
+                    apiBasePath+ "/adoption/applications/create"
                     ).permitAll();
 
 //            // 2. Endpoints Públicos (Según SRS y diseño)
